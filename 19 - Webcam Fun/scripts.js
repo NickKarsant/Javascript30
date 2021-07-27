535786fecce367dfb7d943c5cb40484dea6aac51
Nicholas Karsant <nkarsant@gmail.com>
2021-07-26 22:16:44 -0700

    webcam fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,51 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+
+function getVideo(){
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  .then(localMediaStream => {
+    video.srcObject = localMediaStream;
+    video.play();
+  })
+  .catch( err => {
+    console.error("Oops", err)
+  })
+}
+
+function paintToCanvas(){
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height)
+  }, 50);
+
+}
+
+function takePhoto(){
+  // sound of picture taken
+  snap.currentTime = 0;
+  snap.play()
+
+  // get picture data
+  const data = canvas.toDataURL('image/jpeg')
+  const link = document.createElement('a');
+  link.href = data
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild)
+}
+
+
+
+getVideo();
+
+
+video.addEventListener('canplay', paintToCanvas)
