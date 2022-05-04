cba6308e13362d5dc1ba0511b9e6f7087519bb01
Nicholas Karsant <nkarsant@gmail.com>
2022-05-04 11:38:43 -0700

    finish highlighter

---
+++ 21 - Geolocation/io simulator/Shared/io_simulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  io_simulatorApp.swift
+//  Shared
+//
+//  Created by Nicholas Karsant on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct io_simulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
